b9be5427918e109c258ef0a79c69a94488382494
Jose Vicente Ebri Gomez <aluebr5363@ieselcaminas.org>
2023-12-16 08:25:48 +0100

    Paso a paso finalizado

--- __manifest__.py
+++ __manifest__.py
@@ -3,33 +3,34 @@
     'name': "lapeliculera",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo para SGE""",
 
     'description': """
-        Long description of module's purpose
-    """,
+        No se me actualiza""",
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Jose Vicente Ebri",
+    'website': "https://google.es",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.7',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'reports/informe_pelicula.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    
 }
